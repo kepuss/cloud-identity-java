175576c6808f05c68a60f69203c9f37dc6df48b7
kepuss <michal.kepkowski@gmail.com>
2024-08-09 19:14:06 +1000

    Approles

--- src/main/java/com/example/demo/AzureConfiguration.java
+++ src/main/java/com/example/demo/AzureConfiguration.java
@@ -2,17 +2,16 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.vault.authentication.AzureMsiAuthentication;
-import org.springframework.vault.authentication.AzureMsiAuthenticationOptions;
+import org.springframework.vault.authentication.AppRoleAuthentication;
+import org.springframework.vault.authentication.AppRoleAuthenticationOptions;
 import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.config.AbstractVaultConfiguration;
 
-import java.net.URI;
-
 @Configuration(proxyBeanMethods = true)
 public class AzureConfiguration extends AbstractVaultConfiguration {
 
+    String vaultAuthnType = "approle";
 
     @Override
     public VaultEndpoint vaultEndpoint() {
@@ -21,6 +20,26 @@ public class AzureConfiguration extends AbstractVaultConfiguration {
 
     @Override
     public ClientAuthentication clientAuthentication() {
+        switch (vaultAuthnType) {
+            case "approle":
+                return getAppRoleClientAuthentication();
+            case "azure":
+                return azureCloudClientAuthentication();
+            default:
+                return getAppRoleClientAuthentication();
+        }
+    }
+
+    public ClientAuthentication getAppRoleClientAuthentication() {
+        String roleId = System.getenv("VAULT_ROLE_ID");
+        String secretId = System.getenv("VAULT_SECRET_ID");
+        AppRoleAuthenticationOptions options = AppRoleAuthenticationOptions.builder()
+                .roleId(AppRoleAuthenticationOptions.RoleId.provided(roleId))
+                .secretId(AppRoleAuthenticationOptions.SecretId.provided(secretId)).build();
+        return new AppRoleAuthentication(options, restOperations());
+    }
+
+    public ClientAuthentication azureCloudClientAuthentication() {
         String role = System.getenv("VAULT_ROLE");
         if (role == null) {
             role = "test-role";
--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.vault.annotation.VaultPropertySource;
+import org.springframework.vault.core.VaultTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/test")
@@ -13,8 +19,18 @@ public class DemoController {
     @Autowired
     Environment env;
 
+    @Autowired
+    VaultTemplate vaultTemplate;
+
     @GetMapping("/test")
     public String test() {
         return "user: " + env.getProperty("database.username") + ", password: " + env.getProperty("database.password");
+    }
+
+
+    @PostMapping(value = "/saveSecret",consumes ="application/json")
+    public JsonNode saveSecret(@RequestBody JsonNode secret) {
+        vaultTemplate.write("secret/app/refreshToken", secret);
+        return vaultTemplate.read("secret/app/refreshToken", JsonNode.class).getData();
     }
 }
